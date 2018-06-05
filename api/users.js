aa9f7beb349b5b77794bd273676d5c85fa241465
Anand Kothari <kotharan@oregonstate.edu>
2018-06-04 19:32:53 -0700

    added coded for users and mongo runs successfully

--- api/users.js
+++ api/users.js
@@ -4,6 +4,90 @@ const { getBusinessesByOwnerID } = require('./businesses');
 const { getReviewsByUserID } = require('./reviews');
 const { getPhotosByUserID } = require('./photos');
 
+
+
+// Mongo Code========================================================
+
+// To add user
+function insertNewUser(user,mongoDB) {
+     const usersCollection = mongoDB.collection('users');
+     const userValues = {
+     userID: user.userID,
+     username: user.username,
+     email: user.email,
+     password: user.password
+     };
+     return usersCollection.insertOne(userValues)
+     .then((result) => {
+     return Promise.resolve(result.insertedId);
+     // let p = Promise.resolve(value);
+     let p = new Promise((resolve) => { resolve(value); });
+     });
+     }
+
+     /*
+     * Route to add user
+     */
+
+     router.post('/users', function (req, res) {
+     if (req.body && req.body.userID && req.body.username && req.body.password) {
+          insertNewUser(req.body,req.app.locals.mongoDB)
+            .then((id) => {res.status(201).json({ _id: id });})
+            .catch((err) => {res.status(500).json({
+          error: "Error creating new user."});});
+          } else {
+          res.status(400).json({
+            error: "Request body does not contain valid user data."
+          });
+     }
+     });
+
+// Get user by their userID
+function getUserByuserID(userID,mongoDB) {
+     const usersCollection = mongoDB.collection('users');
+     return usersCollection.find({ userID: userID }).toArray()
+     .then((results) => {
+     return Promise.resolve(results[0]);
+     });
+     }
+     /*
+     * Route to get user info by their userid
+     */
+     router.get('/:userID', function (req, res, next) {
+     getUserByuserID(req.params.userID,req.app.locals.mongoDB)
+       .then((user) => {if (user) {
+            res.status(200).json(user);}
+            else
+            { next();}})
+       .catch((err) => {res.status(500).json({
+       error: "Error fetching user."
+     });});
+     });
+
+//To get print all users
+function getallUsers(mongoDB)
+     {
+     const usersCollection = mongoDB.collection('users');
+     return usersCollection.find().toArray()
+     .then((results) => {
+     return Promise.resolve(results);
+     });
+     }
+
+     // Route to get print all users
+     router.get('/', function (req, res, next) {
+     getallUsers(req.app.locals.mongoDB)
+       .then((userdata) => {if (userdata) {
+            res.status(200).json(userdata);}
+            else
+            { next();}})
+       .catch((err) => {res.status(500).json({
+       error: "Error fetching user."
+     });});
+     });
+
+//=============================================================
+
 /*
  * Route to list all of a user's businesses.
  */
@@ -66,5 +150,6 @@ router.get('/:userID/photos', function (req, res) {
       });
     });
 });
+
 
 exports.router = router;
